9034a6e43f95322fc2c692507f353628d4fc9319
CsCt00 <loza.leandro@gmail.com>
2022-09-12 16:11:54 -0300

    no changes

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 require("./config/db");
 
 app.use(cors());
+//app.use(cors({"origin": "http://localhost:3000/", "credentials": true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());

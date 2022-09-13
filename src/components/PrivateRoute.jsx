3fe64c612d398d182b885539b4da509df25fea6d
albertorapu@gmail.com <albertorapu@gmail.com>
2022-09-13 20:23:07 -0300

    cierra el menú al hacer click en una opción (mobile)

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,11 @@
-import { CircularProgress } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import LoadingScreen from "../commons/LoadingScreen";
 
 export default function PrivateRoute({ children }) {
     const user = useSelector((state) => state.user);
-    if (user.state === "loading") return <CircularProgress />;
+    if (user.state === "loading") return <LoadingScreen />;
     if (user.state === "rejected") return <Navigate to="/" replace={true} />;
     if (user.email) return children;
 }

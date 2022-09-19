c846fa3fdde53909c1a02ddd5264ade505ca0958
CsCt00 <loza.leandro@gmail.com>
2022-09-19 16:06:56 -0300

    added seeds for devices, updated models

---
+++ api/services/measure.services.js
@@ -0,0 +1,33 @@
+const Measure = require("../models/Measures");
+const Device = require("../models/Devices");
+
+class MeasureService {
+  static async seedDb(body) {
+    try {
+      const measure = await Measure.insertMany(body);
+      const device =  await Device.updateOne(
+        {qrCode:measure[0].devEUI},
+       {$push:{measures:measure}});
+      return measure
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  static async create(body) {
+    try {
+      const measure = new Measure(body)
+      return await measure.save();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  static async getAllMeasures(body) {
+    try {
+      return await Measure.find();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = MeasureService;

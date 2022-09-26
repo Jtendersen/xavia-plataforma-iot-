1200505505acf0d1dd32a260ae6b2e141e69fdb2
Jtendersen <jtendersen@hotmail.com>
2022-09-26 15:10:43 -0300

    seeder updated to real values. Battery chart not finished

--- src/components/Aplicaciones.jsx
+++ src/components/Aplicaciones.jsx
@@ -1,13 +1,19 @@
 import { Box } from "@mui/material";
 import React from "react";
+import BatteryUsageChart from "./BatteryUsageChart";
 import DistancePerTimeChart from "./distancePerTimeChart";
 
 const Aplicaciones = () => {
-    return (
-        <Box>
-            <DistancePerTimeChart />
-        </Box>
-    );
+  return (
+    <>
+      <Box>
+        <DistancePerTimeChart />
+      </Box>
+      <Box>
+        <BatteryUsageChart />
+      </Box>
+    </>
+  );
 };
 
 export default Aplicaciones;
---
+++ src/components/BatteryUsageChart.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const BatteryUsageChart = () => {
+  const [userData, setUserData] = useState(false);
+
+  useEffect(() => {
+    axios.get();
+  });
+
+  return <div>BatteryUsageChart</div>;
+};
+
+export default BatteryUsageChart;

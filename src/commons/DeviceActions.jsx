6756e7075876fdb918f414eb06bc13ac00cc0b9c
albertorapu@gmail.com <albertorapu@gmail.com>
2022-09-29 12:56:28 -0300

    devices icon on admin almost done

---
+++ src/commons/DeviceActions.jsx
@@ -0,0 +1,73 @@
+import { useDispatch } from "react-redux";
+import { Button, Dialog, IconButton } from "@mui/material";
+import { Box } from "@mui/system";
+import DeleteIcon from "@mui/icons-material/Delete";
+import React, { useEffect, useState } from "react";
+import PlaylistRemoveIcon from "@mui/icons-material/PlaylistRemove";
+import axios from "axios";
+
+// Botones del dashboard por usuario
+// recibe params de la fila y algunos estados
+const DeviceActions = ({ params, confirmEmpty, setConfirmEmpty, confirmDelete, setConfirmDelete }) => {
+    const dispatch = useDispatch();
+    const [showEmpty, setShowEmpty] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
+
+    const emptyDevice = (name) => {
+        setShowEmpty(true);
+    };
+
+    const deleteDevice = (id) => {
+        setShowDelete(true);
+    };
+
+    const handleClose = () => {
+        setShowDelete(false);
+        setShowEmpty(false);
+    };
+
+    useEffect(() => {
+        if (confirmEmpty) {
+            // axios.delete(`http://localhost:3001/api/device/delete/${params.row.name}`).then(() => {})
+            setShowEmpty(false);
+            setConfirmEmpty(false);
+        }
+        if (confirmDelete) {
+            // axios.delete(`http://localhost:3001/api/measures/delete/${params.id}`).then(() => {})
+            setShowDelete(false);
+            setConfirmDelete(false);
+        }
+    }, [confirmDelete, confirmEmpty, setConfirmDelete, setConfirmEmpty]);
+
+    return (
+        <>
+            <Box>
+                <IconButton>
+                    <PlaylistRemoveIcon onClick={() => emptyDevice(params.row.name)} />
+                </IconButton>
+
+                <IconButton onClick={() => deleteDevice(params.id)}>
+                    <DeleteIcon />
+                </IconButton>
+            </Box>
+
+            {/* render condicional que se activa al clickear el ícono añadir */}
+            {showEmpty && (
+                <Dialog open={showEmpty} onClose={handleClose}>
+                    Confirmar o cancelar acción:
+                    <Button onClick={() => setConfirmEmpty(true)}>Confirmar</Button>
+                    <Button onClick={() => handleClose()}>Cancelar</Button>
+                </Dialog>
+            )}
+            {showDelete && (
+                <Dialog open={showDelete} onClose={handleClose}>
+                    Confirmar o cancelar acción:
+                    <Button onClick={() => setConfirmDelete(true)}>Confirmar</Button>
+                    <Button onClick={() => handleClose()}>Cancelar</Button>
+                </Dialog>
+            )}
+        </>
+    );
+};
+
+export default DeviceActions;
--- src/commons/ShowDevices.jsx
+++ src/commons/ShowDevices.jsx
@@ -1,58 +1,33 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { LinearProgress, Paper, Stack, TextField } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import { Paper } from "@mui/material";
 import { useSelector } from "react-redux";
-import QrScanner from "./QrScanner";
-import qr from "../assets/exampleQR.png";
-import Image from "mui-image";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
-import axios from "axios";
-import { setQrCode } from "../store/reducers/deviceQrCode.reducer";
-import {
-    DataGrid,
-    GridToolbarColumnsButton,
-    GridToolbarContainer,
-    GridToolbarExport,
-    GridToolbarFilterButton,
-    GridToolbarQuickFilter,
-} from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import { getDevices } from "../store/reducers/deviceMeasures.reducer";
 import { getMeasures } from "../store/reducers/getAllMeasures.reducer";
+import DeviceActions from "./DeviceActions";
 
 const style = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    maxWidth: "90%",
     bgcolor: "background.paper",
     border: "border-radius: 45px",
     boxShadow: 24,
     p: 4,
 };
 
-const stackStyle = {
-    direction: "column",
-    spacing: 1,
-    paddingBottom: 0,
-    marginBottom: 0,
-    height: "100%",
-    justifyContent: "space-between",
-};
-
 export default function ShowDevices({ userParams, showDevices, setShowDevices }) {
     const dispatch = useDispatch();
     const measures = useSelector((state) => state.measures);
     const [open, setOpen] = React.useState(true);
+    const [confirmEmpty, setConfirmEmpty] = React.useState(false);
+    const [confirmDelete, setConfirmDelete] = React.useState(false);
+
     const handleClose = () => {
         setOpen(false);
         setShowDevices(false);
@@ -63,36 +38,31 @@ export default function ShowDevices({ userParams, showDevices, setShowDevices })
         dispatch(getMeasures({ entries: 0, user: userParams }));
     }, [dispatch, userParams]);
 
-    const columns = [
-        /*  { field: "id", headerName: "Id", minWidth: 150, flex: 1 }, */
-        { field: "name", headerName: "DevEUI", minWidth: 140, flex: 1 },
-        {
-            field: "lastlat",
-            headerName: "Ultima Medición (Lat)",
-            minWidth: 160,
-            flex: 1,
-        },
-        {
-            field: "lastlon",
-            headerName: "Ultima Medición (Lon)",
-            minWidth: 160,
-            flex: 1,
-        },
-        {
-            field: "mode",
-            headerName: "Modo",
-            minWidth: 160,
-            flex: 1,
-        },
-    ];
+    const columns = React.useMemo(
+        () => [
+            { field: "name", headerName: "DevEUI", minWidth: 140, flex: 0.3 },
+            {
+                field: "measures",
+                headerName: "# Medidas",
+                minWidth: 100,
+                flex: 0.3,
+            },
+            {
+                field: "_id",
+                headerName: "Acciones",
+                type: "actions",
+                flex: 1,
+                renderCell: (params) => <DeviceActions {...{ params, confirmEmpty, setConfirmEmpty, confirmDelete, setConfirmDelete }} />,
+            },
+        ],
+        [confirmDelete, confirmEmpty]
+    );
 
     const rows = Array.isArray(measures)
         ? measures?.map((e, index) => ({
               id: e.length ? e[index]._id : "-",
               name: e.length ? e[index].DevEUI_uplink.DevEUI : "-",
-              lastlat: e.length ? e[0].DevEUI_uplink.LrrLAT : "-",
-              lastlon: e.length ? e[0].DevEUI_uplink.LrrLON : "-",
-              mode: e.length ? e[0].DevEUI_uplink.payload.deviceConfiguration?.mode : "-",
+              measures: e.length ? e.length : "-",
           }))
         : [];
     return (
@@ -111,15 +81,15 @@ export default function ShowDevices({ userParams, showDevices, setShowDevices })
                         flexDirection: "column",
                         borderRadius: "16px",
                     }}
+                    style={style}
                 >
                     {measures ? (
-                        <div style={{ height: 250, width: "100%" }}>
+                        <div style={{ height: 400, width: "100%" }}>
                             <DataGrid sx={{ fontSize: "0.7rem" }} rows={rows} columns={columns} />
                         </div>
                     ) : (
                         <Typography align="center">No hay dispositivos registrados</Typography>
                     )}
-                    3
                 </Paper>
             </Modal>
         </>
